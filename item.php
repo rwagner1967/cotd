6dcb4499676b245c9a3e2cb9560f8ba7e54a1ea0
Roberto Wagner <roberto.wagner@gmail.com>
2019-08-14 21:34:06 -0300

    Change default SELECTOR to cities

--- item.php
+++ item.php
@@ -67,7 +67,7 @@ if ( isset( $favorite ) ) {
 }
 
 $help = "<p>You are looking at a list of ".$_SESSION['SELECTOR'];
-if ( $_SESSION['SELECTOR'] == "cats" ) {
+if ( $_SESSION['SELECTOR'] == "cities" ) {
   $help = $help." from amongst my work colleagues.";
 } else {
   $help = $help. ".";
